97e1d7e30708b04a5d297b9dfd4bd4a55000b4d1
AlexandeR-javaman <posetitel12345@proton.me>
2026-04-10 13:05:03 +0300

    Тесты из микросервисов запускаются, но общие из Maven - нет. Исправил помник и BaseIntegrationTest

--- employees-service/src/test/java/com/example/employees_service/BaseIntegrationTest.java
+++ employees-service/src/test/java/com/example/employees_service/BaseIntegrationTest.java
@@ -9,10 +9,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.transaction.annotation.Transactional;
 
 @SpringBootTest
 @ActiveProfiles("test")
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@Transactional
 public abstract class BaseIntegrationTest {
 
     @Autowired
